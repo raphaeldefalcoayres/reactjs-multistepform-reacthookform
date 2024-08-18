bdbf34c95d6d4a1880bb06dea2cbf1822ac37989
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2024-08-18 09:48:53 -0300

    chore: improvement in logical.

--- src/components/multi-form-step/index.tsx
+++ src/components/multi-form-step/index.tsx
@@ -7,19 +7,16 @@ const Step1 = lazy(() => import('./steps/step1'))
 const Step2 = lazy(() => import('./steps/step2'))
 const Step3 = lazy(() => import('./steps/step3'))
 
-function StepRenderer() {
+function Steps() {
   const { currentStep } = useCustomFormContext()
 
-  switch (currentStep) {
-    case 0:
-      return <Step1 />
-    case 1:
-      return <Step2 />
-    case 2:
-      return <Step3 />
-    default:
-      return null
-  }
+  return (
+    <>
+      {currentStep === 0 && <Step1 />}
+      {currentStep === 1 && <Step2 />}
+      {currentStep === 2 && <Step3 />}
+    </>
+  )
 }
 
 export default function MultiStepForm() {
@@ -27,13 +24,11 @@ export default function MultiStepForm() {
     <CustomFormProvider>
       <section className='absolute inset-0 mx-auto flex flex-col justify-between p-24 lg:max-w-[800px]'>
         <Stepper />
-
         <form className='mt-12 py-12'>
           <Suspense fallback={<div>Loading...</div>}>
-            <StepRenderer />
+            <Steps />
           </Suspense>
         </form>
-
         <Navigation />
       </section>
     </CustomFormProvider>
